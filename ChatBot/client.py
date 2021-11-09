119bc6f06dd75773a602761c00ac2d7d62bb264f
ROCKET19 <48097039+ROCKET19@users.noreply.github.com>
2021-11-09 20:31:04 +0530

    added text to speech response
    The response given by the bot is converted to speech.

--- ChatBot/client.py
+++ ChatBot/client.py
@@ -1,11 +1,17 @@
 import socket
 
+import pyttsx3
+# initialize Text-to-speech engine
+engine = pyttsx3.init()
+engine.setProperty("rate", 180)
+
+
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-
+    print("\nHi I am J.A.R.V.I.S. wLet's chat!!! type 'bei' to exit\n")
     while True:
         sentence = input("You: ")
         if sentence == 'bei':
@@ -15,8 +21,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         data = s.recv(1024).decode()
 
-    # data = s.recv(1024)
         print(data)
+
+        engine.say(data[8:-1])
+        engine.runAndWait()
+
+    # data = s.recv(1024)
+
 
     # exec(open('chat.py').read())
 
